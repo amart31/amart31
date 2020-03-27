960171f04c6a2fc16d5330afe2c161514169469b
Andy Martinez <martinez1751@gmail.com>
2020-03-27 14:28:09 -0400

    updated project structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Home } from './views/Home';
+import { Resume } from './views/Resume';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/resume' component={Resume} />
+      <Route render={() => <h1>Not found!</h1>} />
+    </Switch>
+  </BrowserRouter>
     </div>
   );
 }
---
+++ src/views/Resume.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Resume() {
+  
+  return (
+    <div>
+      <p>Resume page</p>
+    </div>
+  );
+}
