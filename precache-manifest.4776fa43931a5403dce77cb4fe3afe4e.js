0e60253b20a53fd0cb59c56f61ec6dd3b201c724
Andy Martinez <martinez1751@gmail.com>
2020-05-19 21:03:18 -0400

    Updates

---
+++ precache-manifest.4776fa43931a5403dce77cb4fe3afe4e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49391e8272104b1944b168b624cae73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4bf57715c2cd4e15e2c",
+    "url": "/static/css/main.a06fa85a.chunk.css"
+  },
+  {
+    "revision": "f2a050b62330fcf6a7d2",
+    "url": "/static/js/2.bed81555.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bed81555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4bf57715c2cd4e15e2c",
+    "url": "/static/js/main.0515ec9b.chunk.js"
+  },
+  {
+    "revision": "83f3e7696b3a077c4a3d",
+    "url": "/static/js/runtime-main.9d508448.js"
+  },
+  {
+    "revision": "9054008d7af1df4bf73945b17d2ccee4",
+    "url": "/static/media/TCRCaseLifeCycle.9054008d.png"
+  },
+  {
+    "revision": "2d449a4cdc83d18c979d8ac844ad901f",
+    "url": "/static/media/armaNeoCase.2d449a4c.png"
+  },
+  {
+    "revision": "afd3283cdd3518b36db0d91dab0f4840",
+    "url": "/static/media/heroBg-mobil.afd3283c.jpg"
+  },
+  {
+    "revision": "ea090f14e77cdea2e1d422d2cec0f614",
+    "url": "/static/media/heroBg.ea090f14.jpg"
+  },
+  {
+    "revision": "b50b6d7cc93d56c7f6ea116601a2a9ac",
+    "url": "/static/media/react-closet.b50b6d7c.jpg"
+  },
+  {
+    "revision": "d4baa5f915adbf55362d58760e593bf8",
+    "url": "/static/media/trmsPic.d4baa5f9.png"
+  }
+]);
